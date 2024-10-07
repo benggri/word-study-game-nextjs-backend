e8741824e79343f57d44c4c32ce640f72129321a
benggri <benggri@gmail.com>
2024-10-08 07:45:51 +0900

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './node_modules/flowbite/**/*.js',
+    './node_modules/flowbite-react/**/*.{js,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
